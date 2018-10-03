84c8a5a3a152a4738537064d96cb68c8830426a5
mayermediaco <mayermediaco@gmail.com>
2018-10-02 21:40:25 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var express_graphql = require('express-graphql');
+var {buildSchema} = require('graphql');
+
+// GraphQL Schema
+var schema = buildSchema(`
+  type Query {
+    message: String
+  }
+`);
+
+// Root Resolver
+var root = {
+  message: () => 'Hello World!'
+};
+
+// Create express server and a graphql endpoint
+var app = express();
+app.use('/graphql', express_graphql({schema: schema, rootValue: root, graphiql: true}));
+
+app.listen(4000, () => console.log('Express GraphQL Server Now Running on localhost:4000/graphql'));
